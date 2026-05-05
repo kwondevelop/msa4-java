c681b4c77dc4ae0d78a562920ae708b8bf3a27e1
coding_babo <kwondevelop@gmail.com>
2026-05-05 16:49:32 +0900

    star practice

--- src/com/msa4java/edu/HiJava.java
+++ src/com/msa4java/edu/HiJava.java
@@ -1,13 +1,26 @@
 package com.msa4java.edu;
 
+//public class HiJava {
+//    public static void main(String[] args) {
+//        System.out.println(12);
+//        System.out.print(1);
+//        System.out.print(2);
+//
+//        String str = "자바";
+//        int num = 5;
+//        System.out.printf("안녕 %s : %d", str, num);
+//    }
+//}
+
 public class HiJava {
     public static void main(String[] args) {
-        System.out.println(12);
-        System.out.print(1);
-        System.out.print(2);
-        
-        String str = "자바";
-        int num = 5;
-        System.out.printf("안녕 %s : %d", str, num);
+        int i;
+        int j;
+        for (i = 1; i <= 5; i++) {
+            System.out.println("");
+            for (j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+        }
     }
 }
